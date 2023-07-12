b72b8b423e2ad82903ba501aea824ffa9672c71b
Dylan <563657114@qq.com>
2023-07-12 19:08:35 +0800

    完成反馈案例

--- 个人练习/day04/练习2_学习反馈/js/index.js
+++ 个人练习/day04/练习2_学习反馈/js/index.js
@@ -4,3 +4,49 @@
  *  1.2 切换省份，设置城市下拉菜单数据，清空地区下拉菜单
  *  1.3 切换城市，设置地区下拉菜单数据
  */
+function getProvice() {
+    axios({url: 'http://hmajax.itheima.net/api/province'})
+        .then(result => {
+            document.querySelector('.province').innerHTML =
+                '<option value="">省份</option>' +
+                result.data.list.map(item => {
+                    return `<option value=${item}>${item}</option>`
+                }).join('')
+        })
+}
+
+getProvice()
+document.querySelector('.province').addEventListener('change', async (e) => {
+    const cityList = await axios({url: 'http://hmajax.itheima.net/api/city', params: {pname: e.target.value}})
+    document.querySelector('.city').innerHTML =
+        '<option value="">城市</option>' +
+        cityList.data.list.map(item => {
+            return `<option value=${item}>${item}</option>`
+        }).join('')
+    document.querySelector('.area').innerHTML = '<option value="">地区</option>'
+})
+document.querySelector('.city').addEventListener('change', async (e) => {
+    const cityList = await axios({
+        url: 'http://hmajax.itheima.net/api/area', params: {
+            pname: document.querySelector('.province').value,
+            cname: e.target.value
+        }
+    })
+    document.querySelector('.area').innerHTML =
+        '<option value="">地区</option>' +
+        cityList.data.list.map(item => {
+            return `<option value=${item}>${item}</option>`
+        }).join('')
+})
+
+document.querySelector('.submit').addEventListener('click', async () => {
+    const infoForm = document.querySelector('.info-form')
+    const data = serialize(infoForm, {hash: true, empty: false})
+   const result=await axios({
+        url: 'http://hmajax.itheima.net/api/feedback',
+        thead: 'POST',
+        data
+    })
+    console.dir(result)
+    alert(result.data.message)
+})
